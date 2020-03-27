4b6c0e4e7d78a5f7fe718361c7753ac0c1063dd6
Asad <aliasad71734@gmail.com>
2020-03-27 17:51:41 +0500

    all files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const mustacheExpress = require('mustache-express');
+const bodyparser = require('body-parser');
 
 const app = express();
 const mustache = mustacheExpress();
@@ -11,6 +11,16 @@ app.set('view engine','mustache');
 
 
 app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.get('/add',(req,res)=>{
+    res.render('med-form');
+});
+
+app.post('/meds/add',(req,res)=>{
+    console.log('post body',req.body);
+     res.redirect('/meds');
+});
 
 app.get('/meds',(req,res)=>{
     res.render('meds');
